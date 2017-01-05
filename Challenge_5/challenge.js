363cb082c5cf8f73b1e0e6b17303cfacb9c33b8c
Annie Zhang <harveydoodle@users.noreply.github.com>
2017-01-04 20:59:50 -0500

    Challenge 5

---
+++ Challenge_5/challenge.js
@@ -0,0 +1,40 @@
+//Ex. 1
+var name = "Annie";
+    console.log("Hello " + name);
+
+//Ex.2 
+var x = 1125;
+var answer = x % 2;
+    console.log(answer);
+
+//Ex. 3
+
+a = true;
+b = false;
+c = true; 
+    console.log(a || b || c);
+
+//Ex. 4
+    console.log(a && b && c); 
+
+//Ex. 5
+var answer1 = 10 - 3 * 5 / 4 + 202 % 3;
+var answer2 = 10 - (3 * (5 / 4) + (202 % 3));
+    console.log(answer1);
+    console.log(answer2);
+
+//Ex. 6
+int = 12;
+var intString = String(int);
+    console.log(intString)
+
+str = 113;
+var strInt = parseInt(str);
+    console.log(strInt);
+
+flt = 1.12356;
+var strFlt = parseFloat(flt)
+    console.log(strFlt);
+
+var x = parseInt("Hello World");
+    console.log(x);
