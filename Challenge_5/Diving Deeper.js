1b01ebee842f65be20627a1e48d46a6be1b3a1d2
Annie Zhang <harveydoodle@users.noreply.github.com>
2017-01-10 01:49:55 -0500

    Diving Deeper.js

--- Challenge_5/Diving Deeper.js
+++ Challenge_5/Diving Deeper.js
@@ -3,10 +3,10 @@ var n = str.search("BrainStation");
 console.log(n);
 
 
-function functionTwo(){
-var re = /(\w+)\s(\w+)/;
-var str = "John Smith";
-var newstr = str.replace(re, "$2, $1");
-console.log(newstr);
+var str = "I am a student"; 
+var n = str.search("BrainStation");
+console.log(n);
 
-}
+var str = "I am a BrainStation student";
+var n = str.replace(/BrainStation student/i, 'Junior Developer');
+console.log(n);
